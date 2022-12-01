acdb3770a37f5dd397de7d230c1bd744847e8068
Yorki <yorki.ogame@gmail.com>
2022-12-02 00:18:27 +0100

    Register facade directly from service provider

--- src/SettingsServiceProvider.php
+++ src/SettingsServiceProvider.php
@@ -2,10 +2,24 @@
 
 namespace Npabisz\LaravelSettings;
 
+use Npabisz\LaravelSettings\Facades\Accessors\SettingsAccessor;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 
 class SettingsServiceProvider extends ServiceProvider
 {
+    /**
+     * Register Settings Facade
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('settings', function ($app) {
+            return new SettingsAccessor(Auth::user());
+        });
+    }
+
     /**
      * Bootstrap any application services.
      *
@@ -29,10 +43,6 @@ class SettingsServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../database/migrations' => database_path('migrations'),
             ], 'settings-migrations');
-
-            $this->publishes([
-                __DIR__.'/../stubs/SettingsServiceProvider.stub' => app_path('Providers/SettingsServiceProvider.php'),
-            ], 'settings-provider');
 
             $this->publishes([
                 __DIR__.'/../stubs/Setting.stub' => app_path('Models/Setting.php'),
